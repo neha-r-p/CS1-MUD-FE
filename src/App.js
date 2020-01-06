e4af2cd758f9795b56001df0ebb50ac9849d9d29
AlexFox1777 <mix.filipov@mail.ru>
2020-01-06 13:00:16 -0800

    simple navbar with styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+//Router config
+import {
+    Switch,
+    Route
+} from 'react-router-dom'
+//Components
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+
 
 function App() {
-  return (
-    <div className="App">
-        Adventure game!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar/>
+            Adventure game!
+            <Switch>
+                {/* {public routes} */}
+                <Route exact path={"/"} component={Home}/>
+                <Route path={"/login"} component={Login}/>
+                <Route path={"/register"} component={Register}/>
+
+                {/* {private routes} */}
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+//Router config
+import {BrowserRouter as Router} from 'react-router-dom'
+//Theme
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {createMuiTheme, responsiveFontSizes, ThemeProvider} from '@material-ui/core/styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//Theme config
+let theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#fe0014",
+        },
+        secondary: {
+            main: "#fff7f7",
+        }
+    }
+});
+
+theme = responsiveFontSizes(theme);
+
+
+ReactDOM.render(
+    <Router>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <App/>
+        </ThemeProvider>
+    </Router>,
+    document.getElementById('root'))
+
+serviceWorker.unregister()
