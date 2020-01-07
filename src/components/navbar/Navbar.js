e6e936875fbea498cbd2d4558f545c3e8a865e37
Neha Patel <neha.r.p@gmail.com>
2020-01-07 14:59:39 -0800

    replaced react logo with cute characters

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Link, withRouter} from "react-router-dom";
 //Components
-import Avatar from "@material-ui/core/Avatar";
+// import Avatar from "@material-ui/core/Avatar";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 //Styles
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import Button from "@material-ui/core/Button";
+//logo
+import creatures from "../../assets/creatures3_.png"
 
 const useStyles = makeStyles({
     root: {
@@ -26,11 +28,11 @@ function Navbar(props) {
         <div className={classes.root}>
             <AppBar>
                 <Toolbar variant="dense">
-                    <Avatar alt="Logo" src={'/logo512.png'}/>
+                    <img src={creatures} alt="Logo" />
                     <Typography variant={"h5"}
                                 className={classes.title}
                                 onClick={() => props.history.push("/")}>
-                        Logo
+                        The Adventuriest Adventure Game
                     </Typography>
                     <Button component={Link} to={'/login'} color={"secondary"}>Login</Button>
                     <Button component={Link} to={'/register'} color={"secondary"}>Register</Button>
