245d31ad61c0473e0c3fe8e38f1b2607511a5932
Neha Patel <neha.r.p@gmail.com>
2020-01-15 21:58:18 -0800

    updated styles for room info

--- src/components/inventory/RoomInfo.js
+++ src/components/inventory/RoomInfo.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 //styles
 import makeStyles from '@material-ui/core/styles/makeStyles'
 //store
@@ -6,40 +6,42 @@ import { connect } from 'react-redux'
 import { currentRoom } from '../../store/player/playerActions'
 
 const useStyles = makeStyles({
-    root: {
-        color: 'white',
-        height: '130px',
-        backgroundColor: '#F7B801'
-    },
-    roomName: {
-        margin: '5px auto'
-    },
-    roomDescription: {
-        margin: '10px auto',
-        fontSize: '16px',
-        fontWeight: 'bold'
-    }
+  root: {
+    color: 'white',
+    height: '130px',
+    backgroundColor: '#eb9b07'
+  },
+  roomName: {
+    margin: '5px auto',
+  },
+  roomDescription: {
+    margin: '7px 5px 10px',
+    fontSize: '16px',
+    fontWeight: '600',
+    textAlign: 'left'
+  }
 })
 
 const RoomInfo = props => {
-    const classes = useStyles()
-    useEffect(() => {
-        props.currentRoom(props.player)
-    },[props.player.position])
+  const classes = useStyles()
+  useEffect(() => {
+    props.currentRoom(props.player)
+  }, [props.player.position])
 
-    return (
-        <div className={classes.root}>
-            <h3 className={classes.roomName}>{props.player.title}:</h3>
-            <p className={classes.roomDescription}>{props.player.description}</p>
-        </div>
-    )
+  const roomTitle = props.player.title
+  return (
+    <div className={classes.root}>
+      <h3 className={classes.roomName}>{roomTitle.toUpperCase()}</h3>
+      <p className={classes.roomDescription}>{props.player.description}</p>
+    </div>
+  )
 }
 
 const mapStateToProps = state => {
-    return {
-        player: state.player,
-        rooms: state.map.rooms
-    }
+  return {
+    player: state.player,
+    rooms: state.map.rooms
+  }
 }
 
 export default connect(mapStateToProps, { currentRoom })(RoomInfo)
